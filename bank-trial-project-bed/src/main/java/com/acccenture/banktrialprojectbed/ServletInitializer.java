1bed833dde0d104fe5e9d49b74bee2d6c72924f7
user-marvin <villamar.marvin.b.8138@gmail.com>
2023-01-23 14:42:27 +0800

    My First Commit

---
+++ bank-trial-project-bed/src/main/java/com/acccenture/banktrialprojectbed/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.acccenture.banktrialprojectbed;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BankTrialProjectBedApplication.class);
+	}
+
+}
---
+++ bank-trial-project-bed/src/test/java/com/acccenture/banktrialprojectbed/BankTrialProjectBedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acccenture.banktrialprojectbed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTrialProjectBedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
