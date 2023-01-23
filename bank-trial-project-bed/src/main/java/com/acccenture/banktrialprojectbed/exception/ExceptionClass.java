322774ecdd257fc5a07acd4608419dedeb474fa1
user-marvin <villamar.marvin.b.8138@gmail.com>
2023-01-23 16:15:41 +0800

    working with backend

---
+++ bank-trial-project-bed/src/main/java/com/acccenture/banktrialprojectbed/exception/ExceptionClass.java
@@ -0,0 +1,9 @@
+package com.acccenture.banktrialprojectbed.exception;
+
+public class ExceptionClass extends Exception{
+
+    public static final String ACCOUNT_DO_NOT_EXIST = "Account doesn't exist.";
+    public static final String ACCOUNT_CREATED = "Account created successfully.";
+    public static final String ACCOUNT_UPDATED = "Account has been updated.";
+    public static final String ACCOUNT_DELETED = "Account has been deleted.";
+}
